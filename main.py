162b09a805fca89846d8cda8cda258b73bd1e8dc
Ruben Bes <rubenbes94@gmail.com>
2019-03-11 11:27:14 +0100

    using multi search and threads to speed up search

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import numpy as np
 import re
 import time
+from multiprocessing.dummy import Pool as ThreadPool
+import io
 
 # 1. Goal: Represent the raw content of the query/table as a set of terms, where terms are entities from a knowledge base. {q1, ...., 1n} for query q and {t1, ..., tm} for table T.
 # 1.1. Detect Core Column, this column has the highest ratio of entities
@@ -32,55 +34,72 @@ def detect_column_entities(table, column):
     for row in table:
         text = row[column]
         if re.search("\[.+\|.+\]", text):  # entities are listed as [this_is_an_entity|entity], use regex to detect
-            entities |= detect_entities(text[1:-1].split("|")[1])  # use 0 for entity identifier, 1 for entity name
+            entities.add(text[1:-1].split("|")[1])  # use 0 for entity identifier, 1 for entity name
     return entities
 
 
 # Detect entities in a text using Elasticsearch index on dbpedia
-def detect_entities(text):
+def detect_entities(entity_set):
     entities = set([])  # using a set so duplicates are not added
     es = Elasticsearch()
     index = "dbpedia_2015_10"
 
     # TODO include MLM, this will very likely improve the results
-    query = {
-        "sort": ["_score"],
-        "query": {
-            "match": {
-                "names": text
-            }
-        }
-    }
-    res = es.search(index, body=query)
-    # print("Got %d Hits" % res['hits']['total'])
-    for hit in res['hits']['hits']:
-        # print(hit['_source'].keys())
-        entities |= set(hit['_source']['names'])
+    search_array = []
+    for entity in entity_set:
+        search_array.append({'index': index, "sort": ["_score"]})
+        # search_array.append({"query": {"match": {"names": entity}}})
+        search_array.append({"query": {"multi_match": {"query": entity, "fields": ["names"]}}})
+
+    request = ''
+    for each in search_array:
+        request += '%s \n' % json.dumps(each)
+    res = es.msearch(body=request, max_concurrent_searches=1000)
+    # print(res)
+    # # print("Got %d Hits" % res['hits']['total'])
+    for response in res['responses']:
+        for hit in response['hits']['hits']:
+            # print(hit['_source'].keys())
+            entities |= set(hit['_source']['names'])
     # print("%d entities related to %s" % (len(entities), text))
     # print(entities)
     return entities
 
 
-with open("data/tables/re_tables-0001.json", 'r') as tables, open("data/tables/table-core.json", 'r') as core:
-    tables_json = json.load(tables)
+def retrieve(table_name):
+    _t = table_name
+    table = tables_json[_t]
+    if not (table['numDataRows'] == 0 or table['numCols'] == 0):
+        text_entities = set([])  # using a set so duplicates are not added
+        page_title = table['pgTitle']  # use this to extract entities as well
+        # print(page_title)
+        text_entities.add(page_title)
+        table_caption = table['caption']  # use this to extract entities as well
+        # print(table_caption)
+        text_entities.add(table_caption)
+        main_col = table_core[_t]
+        # print("Main column is number %d, which is named %s" % (main_col, table['title'][main_col]))
+        # next step is to extract entities from main column
+        text_entities |= detect_column_entities(table['data'], main_col)
+        # print(entities)
+        # print("Processed table %s" % _t)
+        return detect_entities(text_entities)
+    return None
+
+
+with io.open("data/tables/table.json", 'r') as tables, open("data/tables/table-core.json", 'r') as core:
+    pool = ThreadPool(1)
+    all_entities = []
     table_core = json.load(core)
     start = time.clock()
-    for t in tables_json:
-        table = tables_json[t]
-        # print(table)
-        # check for empty table
-        if not(table['numDataRows'] == 0 or table['numCols'] == 0):
-            entities = set([])  # using a set so duplicates are not added
-            page_title = table['pgTitle']  # use this to extract entities as well
-            # print(page_title)
-            entities |= detect_entities(page_title)
-            table_caption = table['caption']  # use this to extract entities as well
-            # print(table_caption)
-            entities |= detect_entities(table_caption)
-            main_col = table_core[t]
-            # print("Main column is number %d, which is named %s" % (main_col, table['title'][main_col]))
-            # next step is to extract entities from main column
-            entities |= detect_column_entities(table['data'], main_col)
-            # print(entities)
+    counter = 0
+    for line in tables:
+        tables_json = json.loads(line)
+        all_entities += pool.map(retrieve, tables_json.keys())
+        counter += 1
+        if counter % 100 == 0:
+            print("processed %d tables" % counter)
+            print(len(all_entities))
     end = time.clock()
-    print("Single table file took %s seconds" % (end-start))
+    print("Entire file took %s seconds" % (end-start))
+    print(len(all_entities))
